fb670286c1ecf3f58e614565b180669ceb6b6bff
Jija-sarak <90369836+Jija-sarak@users.noreply.github.com>
2022-02-04 09:48:22 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,34 @@
+# a  = open("people1.txt","w")
+# a.write("""priyanka - shimla
+#         neela - delhi
+#         sashi - jaipur
+#         sarika - delhi
+#         deepti - shimla
+#         harshad - delhi
+#         trishna - delhi
+#         pradeep - jaipur
+#         sekhar - delhi
+#         nand - delhi
+#         anoop - delhi
+#         balwinder - jaipur""")
+# a.close()
+# a = open("people1.txt")
+# file_data = a.read()
+# print (file_data)
+# a.close()
+
+# batch1_students = ["Shivam", "Rahul", "Kavay", "Dhannu", "Deepanshu", "Nitin", "Manoj", "Shakrudin", "Tara", "Suraj", "Krishna"]
+# students_file = open("navgurukul_students.html", "w")
+# students_file.write("\n")
+# students_file.write("\n")
+# students_file.write("\n")
+# students_file.write("\n")
+# students_file.write("\n")
+# students_file.write("\n")
+# for student in batch1_students:
+#     students_file.write("" + student + "\n")
+#     students_file.write("\n")
+#     students_file.write("\n")
+#     students_file.write("\n")
+# students_file.close()
+
